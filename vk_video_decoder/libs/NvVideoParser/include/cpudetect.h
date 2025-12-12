4f4ccb0f03984299a25abf242c30795b9cb652cd
Piers Daniell <pdaniell@nvidia.com>
2025-12-12 14:33:43 -0700

    Get count_trailing_zeros() to compile for MSVC 32-bit

--- vk_video_decoder/libs/NvVideoParser/include/cpudetect.h
+++ vk_video_decoder/libs/NvVideoParser/include/cpudetect.h
@@ -13,11 +13,19 @@ enum SIMD_ISA
 
 static int inline count_trailing_zeros(unsigned long long resmask)
 {
-#ifndef _WIN32
-    int offset = __builtin_ctzll(resmask);
-#else
+#ifdef _WIN64
     unsigned long offset = 0;
     const unsigned char dummyIsNonZero =_BitScanForward64(&offset, resmask); // resmask can't be 0 in this if
+#elif _WIN32
+    unsigned long offset = 0;
+    const unsigned char isNonZero = _BitScanForward(&offset, (unsigned long)(resmask >> 32U)); // resmask can't be 0 in this if
+    if (isNonZero) {
+        offset += 32U;
+    } else {
+        _BitScanForward(&offset, (unsigned long)resmask); // resmask can't be 0 in this if
+    }
+#else
+    int offset = __builtin_ctzll(resmask);
 #endif
     return offset;
 }
